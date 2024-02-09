d2bda8ffecc7ba9263ccd91b9806e5df145e1bad
artur <artur_pfeifer2@gmx.de>
2024-02-10 00:45:56 +0100

    .

--- src/shortGptVideoCreator/randomFactsVideoCreator.js
+++ src/shortGptVideoCreator/randomFactsVideoCreator.js
@@ -1,1 +1,31 @@
 //TODO: Implement this function
+
+
+// short 
+// /html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[2]/div/div[3]/fieldset/div[2]/label[1]/input
+
+//custom facts
+// /html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[2]/div/div[5]/div/div[1]/fieldset[1]/div[2]/label[4]/input
+
+// type into text area
+// /html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[2]/div/div[5]/div/div[1]/div[2]/label/textarea
+
+
+// eleven labs 
+// /html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[2]/div/div[5]/div/div[1]/fieldset[2]/div[2]/label[1]/input
+
+// vs
+
+// edge tts
+// /html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[2]/div/div[5]/div/div[1]/fieldset[2]/div[2]/label[2]/input
+
+
+
+// use images checkbox
+
+// number of images
+
+// background video
+
+// background music
+
